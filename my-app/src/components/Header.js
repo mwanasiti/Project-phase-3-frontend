596e619188ad72e0daf842b8238a8eaa74470a26
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-09 00:59:47 +0300

    milestone:

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -3,7 +3,14 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      
+      <header className='App-header'>
+        <div className="image">
+        <img src='https://media.istockphoto.com/videos/pink-dust-falling-onto-a-black-surface-and-creating-a-female-gender-video-id1269886110?s=640x640'></img>
+      </div>  
+        <div>
+      <h1>PRO</h1>
+      </div>
+      </header>
     </div>
   )
 }
