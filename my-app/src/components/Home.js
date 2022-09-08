eadbf84c397fecb891512ed95d0c037f58120695
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-09 01:33:34 +0300

    milestone:

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -30,49 +30,16 @@ const Home = () => {
     <div className="carousel-item active">
       <img src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp" className="d-block w-100" alt="Wild Landscape"/>
       <div className="carousel-caption d-none d-md-block">
-      <div className="image">
-        <img className='fadeimg' src='https://media.istockphoto.com/videos/pink-dust-falling-onto-a-black-surface-and-creating-a-female-gender-video-id1269886110?s=640x640'></img>
-      </div>  
-        <div>
-      <h2>PRO</h2>
-      </div>
-      <div className='buttons'>
-      <button type="button" className="btn btn-outline-primary btn-rounded" data-mdb-ripple-color="dark">Courses</button>
-<button type="button" className="btn btn-outline-secondary btn-rounded" data-mdb-ripple-color="dark">Topics</button>
-<button type="button" className="btn btn-outline-success btn-rounded" data-mdb-ripple-color="dark">Authors</button>
-      </div>
       </div>
     </div>
     <div className="carousel-item">
       <img src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp" className="d-block w-100" alt="Camera"/>
       <div className="carousel-caption d-none d-md-block">
-      <div className="image">
-        <img className='fadeimg' src='https://media.istockphoto.com/videos/pink-dust-falling-onto-a-black-surface-and-creating-a-female-gender-video-id1269886110?s=640x640'></img>
-      </div>  
-        <div>
-      <h2>PRO</h2>
-      </div>
-      <div className='buttons'>
-      <button type="button" className="btn btn-outline-primary btn-rounded" data-mdb-ripple-color="dark">Courses</button>
-<button type="button" className="btn btn-outline-secondary btn-rounded" data-mdb-ripple-color="dark">Topics</button>
-<button type="button" className="btn btn-outline-success btn-rounded" data-mdb-ripple-color="dark">Authors</button>
-      </div>
       </div>
     </div>
     <div className="carousel-item">
       <img src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp" className="d-block w-100" alt="Exotic Fruits"/>
       <div className="carousel-caption d-none d-md-block">
-      <div className="image">
-        <img className='fadeimg' src='https://media.istockphoto.com/videos/pink-dust-falling-onto-a-black-surface-and-creating-a-female-gender-video-id1269886110?s=640x640'></img>
-      </div>  
-        <div>
-      <h2>PRO</h2>
-      </div>
-      <div className='buttons'>
-      <button type="button" className="btn btn-outline-primary btn-rounded" data-mdb-ripple-color="dark">Courses</button>
-<button type="button" className="btn btn-outline-secondary btn-rounded" data-mdb-ripple-color="dark">Topics</button>
-<button type="button" className="btn btn-outline-success btn-rounded" data-mdb-ripple-color="dark">Authors</button>
-      </div>
       </div>
     </div>
   </div>
