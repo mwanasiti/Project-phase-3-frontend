2fcc232a93615111a7db4bce6aba3eacd86c4d08
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-09 00:42:49 +0300

    milestone:

--- my-app/src/components/Courses.js
+++ my-app/src/components/Courses.js
@@ -14,9 +14,7 @@ function Courses ({courses}){
                 <div>Author: {course.author_id}</div>
                 <div>Course link: <a href={course.link}>Tap to view course</a></div>
 
-                <!-- Add icon library -->
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+                
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
                 <span class="fa fa-star checked"></span>
