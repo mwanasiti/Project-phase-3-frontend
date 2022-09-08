d4796bd73c6a88c54164be89adcff1f1691b44a6
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-08 11:26:08 +0300

    milestone: added necessary input in home component

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <div className="image">
         <img src='https://media.istockphoto.com/videos/pink-dust-falling-onto-a-black-surface-and-creating-a-female-gender-video-id1269886110?s=640x640'></img>
       </div>  
         <div>
       <h2>PRO</h2>
       </div>
+      <div className="buttons">
+                <Button>Leadership</Button>
+                <Button>Human rights</Button>
+                <Button>Activism</Button>
+                </div>
     </div>
   )
 }
