c617f95da57763b32a80363e326988f2020b5504
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-09 00:39:39 +0300

    milestone:

--- my-app/src/components/Courses.js
+++ my-app/src/components/Courses.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 
-
-
 function Courses ({courses}){
     return (  
         <div className="course">
@@ -15,6 +13,15 @@ function Courses ({courses}){
                 <div>Category: {course.topic_id}</div>
                 <div>Author: {course.author_id}</div>
                 <div>Course link: <a href={course.link}>Tap to view course</a></div>
+
+                <!-- Add icon library -->
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
                     </div>
                     </div>
                     </div>
