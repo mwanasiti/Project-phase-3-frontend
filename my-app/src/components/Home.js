ad39fce2d098e3543fdadeca8f5168a9fddc48c1
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-08 11:41:38 +0300

    milestone: edited home component

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className='homepage'>
       <div className="image">
         <img src='https://media.istockphoto.com/videos/pink-dust-falling-onto-a-black-surface-and-creating-a-female-gender-video-id1269886110?s=640x640'></img>
       </div>  
         <div>
       <h2>PRO</h2>
       </div>
-      <div className="buttons">
-                <Button>Leadership</Button>
-                <Button>Human rights</Button>
-                <Button>Activism</Button>
-                </div>
+      <div className='buttons'>
+      <button type="button" class="btn btn-outline-primary btn-rounded" data-mdb-ripple-color="dark">Courses</button>
+<button type="button" class="btn btn-outline-secondary btn-rounded" data-mdb-ripple-color="dark">Topics</button>
+<button type="button" class="btn btn-outline-success btn-rounded" data-mdb-ripple-color="dark">Authors</button>
+      </div>
     </div>
   )
 }
