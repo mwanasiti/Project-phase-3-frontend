427b1d543af56faa87b036b6abfef5f5d84a6163
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-08 17:48:11 +0300

    milestone:

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import image from './about.jpg'
+import image from './project.jpg'
 
 function About() {
   return (
@@ -14,14 +14,15 @@ function About() {
       <div>
         <p className="paragraph">
           <p>`I am of the opinion that my life belongs to the whole community and as long as I live, it is my privilege to do for it whatever I can.` _George B.S_</p>
-          <p>Around 2.4 billion women of working age are not afforded equal economic opportunity and 178 countries maintain legal barriers that prevent their full economic participation, according to the World Bank’s Women, Business and the Law 2022 report. In 86 countries, women face some form of job restriction and 95 countries do not guarantee equal pay for equal work.
+          <p>Around 2.4 billion women of working age are not afforded equal economic opportunity and 178 countries maintain legal barriers that prevent their full economic participation, according to the World Bank's Women, Business and the Law 2022 report. In 86 countries, women face some form of job restriction and 95 countries do not guarantee equal pay for equal work.
 
-</p>
+</p>“While progress has been made, the gap between men's and women's expected lifetime earnings globally is US$172 trillion - nearly two times the world's annual GDP,” said Mari Pangestu, World Bank Managing Director of Development Policy and Partnerships. “As we move forward to achieve green, resilient and inclusive development, governments need to accelerate the pace of legal reforms so that women can realize their full potential and benefit fully and equally.”
           <p>
 </p>
-          <p>_Sustained momentum in public sector reforms_
-Since 1980s, the government has seen a renewed momentum in improving service delivery. The civil service reform program was adopted in early 1990s aimed to build a public service workforce that was responsive to the needs of the citizens. Under the economic recovery strategy, the reform agenda includes efforts to nurture and instil attitudinal change in the public service. Further, the Kenya Vision 2030 identifies public sector reforms as key in the transformative agenda with expectation that public service is citizen-focused and results-oriented institutions in serving a rapidly growing economy and society.</p>
-          <p>This app plays the digital role of further enabling improvement of public service delivery quality through data collection of citizen feedback.</p>
+          <p>WASHINGTON, Mar. 1, 2022. Around 2.4 billion women of working age are not afforded equal economic opportunity and 178 countries maintain legal barriers that prevent their full economic participation, according to the World Bank's Women, Business and the Law 2022 report. In 86 countries, women face some form of job restriction and 95 countries do not guarantee equal pay for equal work.
+</p>
+          <p>Creating an app that would help further achieve this SDG, Sustainable Development Goal, by combining E-learning, Training of trainers courses, Digital solution and in overall Advocacy. This app would enable access to related source material.
+</p>
 
 </p>
 
