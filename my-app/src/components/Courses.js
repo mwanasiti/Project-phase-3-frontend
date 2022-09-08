66cff4a4f0a3cbc965d73c29a15453f765301569
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-08 22:39:06 +0300

    milestone:

--- my-app/src/components/Courses.js
+++ my-app/src/components/Courses.js
@@ -14,8 +14,8 @@ function Courses ({courses}){
                 <div>{course.name}</div>
                 <div>{course.topic_id}</div>
                 <div>{course.author_id}</div>
-                <div><a href= "#">{"https://gateway.ipfs.io/ipfs/"+this.state.ipfsHash}</a></div>
-                {course.link}
+                <div><a href= "#">{course.link}</a></div>
+                {/* {course.link} */}
                     </div>
                     </div>
                     </div>
