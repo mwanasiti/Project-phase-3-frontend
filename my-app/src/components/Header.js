90ec39ebc72936ba855495942d5d1e4c52dc3e48
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-09 01:20:52 +0300

    milestone:

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -4,12 +4,14 @@ const Header = () => {
   return (
     <div>
       <header className='App-header'>
-        <div className="image">
-        <img src='https://media.istockphoto.com/videos/pink-dust-falling-onto-a-black-surface-and-creating-a-female-gender-video-id1269886110?s=640x640'></img>
+        <section className='headinggg'>
+        <div>
+        <img className="headerimage" src='https://media.istockphoto.com/videos/pink-dust-falling-onto-a-black-surface-and-creating-a-female-gender-video-id1269886110?s=640x640'></img>
       </div>  
         <div>
-      <h1>PRO</h1>
+      <h2>PRO</h2>
       </div>
+      </section>
       </header>
     </div>
   )
