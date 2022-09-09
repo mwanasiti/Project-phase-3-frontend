a781c347abb56cd770d3ed4dc7f8840a70a1ca1f
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-09 03:06:37 +0300

    milestone:

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className='homepage'>
       <section className='homeimage'>
-      <div className="image">
+      <div className="">
         <img className='' src='https://media.istockphoto.com/videos/pink-dust-falling-onto-a-black-surface-and-creating-a-female-gender-video-id1269886110?s=640x640'></img>
       </div>  
         <div>
